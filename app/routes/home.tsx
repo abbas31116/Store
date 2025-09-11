1017dc3e3ffca864cc38d4f7fbb8bdf3384e212c
pooris sharifi <pooriasharifi577@gmail.com>
2025-09-11 17:03:17 +0330

    ...

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,67 +1,80 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Product from "./products";
 import CustomButton1 from "~/components/CustomButton";
 import { CustomInput } from "~/components/CustomInput";
 
 export default function Home() {
   const [count, setCount] = useState(0);
-  const [values, setValues] = useState<string>();
   const image =
     "https://cdpcdn.dx1app.com/products/USA/YA/2025/MC/SUPERSPORT/YZF-R3/50/MATTE_STEALTH_BLACK/2000000003.jpg";
-  const products = [
-    { name: "yamaha R3", price: 1, id: 1 },
-    { name: "yamaha R3", price: 2, id: 2 },
-    { name: "yamaha R3", price: 3, id: 3 },
-    { name: "yamaha R3", price: 4, id: 4 },
-    { name: "yamaha R3", price: 5, id: 5 },
-    { name: "yamaha R3", price: 6, id: 6 },
-    { name: "yamaha R3", price: 7, id: 7 },
-    { name: "yamaha R3", price: 8, id: 8 },
-    { name: "yamaha R3", price: 9, id: 9 },
-    { name: "yamaha R3", price: 10, id: 10 },
-    { name: "yamaha R3", price: 11, id: 11 },
-    { name: "yamaha R3", price: 12, id: 12 },
-  ];
+  const [products, setProducts] = useState([
+    { name: "yamaha R3", price: 1, id: 1, stock: "existing" },
+    { name: "yamaha R3", price: 2, id: 2, stock: "existing" },
+    { name: "yamaha R3", price: 3, id: 3, stock: "existing" },
+    { name: "yamaha R3", price: 4, id: 4, stock: "existing" },
+    { name: "yamaha R3", price: 5, id: 5, stock: "existing" },
+    { name: "yamaha R3", price: 6, id: 6, stock: "existing" },
+    { name: "yamaha R3", price: 7, id: 7, stock: "NonExisting" },
+    { name: "yamaha R3", price: 8, id: 8, stock: "NonExisting" },
+    { name: "yamaha R3", price: 9, id: 9, stock: "NonExisting" },
+    { name: "yamaha R3", price: 10, id: 10, stock: "NonExisting" },
+    { name: "yamaha R3", price: 11, id: 11, stock: "NonExisting" },
+    { name: "yamaha R3", price: 12, id: 12, stock: "NonExisting" },
+  ]);
+  const [temp, settemp] = useState<any>([]);
+
   console.log("render root");
   const handleClick = useCallback((name: string, price: number) => {
     console.log(`${name} be gheymat ${price} add to cart shod`);
   }, []);
 
   const handleChange = (value: React.ChangeEvent<HTMLInputElement>) => {
-    setValues(value.target.value);
+    if (value.target.value) {
+      setProducts(
+        temp.filter((item: any) => {
+          return item.price <= parseInt(value.target.value);
+        })
+      );
+    } else {
+      setProducts(temp);
+    }
   };
-  // const FilterGheymat=()=>{
-  // if(products.filter((item,index)=>index==values)){
 
-  // }
-  // }
+  useEffect(() => {
+    settemp(products);
+  }, []);
+
+  const handleExist = () => {
+    setProducts(
+      temp.filter((item: any) => {
+        return item.stock === "existing";
+      })
+    );
+  };
   return (
     <div className="p-5 ">
-      <CustomInput
-        placeHolder={"Filter Gheymat Cala"}
-        title={"Filter"}
-        onChange={handleChange}
-      />
+      <div className="flex items-center gap-4">
+        <CustomInput
+          placeHolder={"Filter Gheymat Cala"}
+          title={"Filter"}
+          onChange={handleChange}
+        />
+        <CustomButton1 title="exist" onClick={handleExist} />
+      </div>
       <div className="grid grid-cols-5  ">
-        {products
-          .filter((item, index) => {
-            if (values) {
-              return item.price <= Number(values);
-            }
-            return item;
-          })
-          .map((item, index) => {
-            return (
-              <div key={index}>
-                <Product
-                  image={image}
-                  name={item.name}
-                  price={item.price}
-                  onclick={handleClick}
-                />
-              </div>
-            );
-          })}
+        {products.map((item, index) => {
+          return (
+            <div key={index}>
+              <Product
+                exist={item.stock}
+                image={image}
+                name={item.name}
+                price={item.price}
+                onclick={handleClick}
+              />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -1,22 +1,22 @@
-import { memo } from "react"
-import CustomButton1 from "~/components/CustomButton"
+import { memo } from "react";
+import CustomButton1 from "~/components/CustomButton";
 
-interface IProducts{
-    image:string,
-    name:string
-    price:number,
-    onclick:(name:string,price:number)=>void
+interface IProducts {
+  image: string;
+  name: string;
+  price: number;
+  exist: string;
+  onclick: (name: string, price: number) => void;
 }
-function Product({image,name,price,onclick}:IProducts) {
-          return (
-            <div className="border rounded-2xl p-3 m-3 place-content-center place-items-center">
-              <img src={image} alt="s" />
-              <p>
-                {name}
-              </p>
-              <p>{price}</p>
-              <CustomButton1 onClick={()=>onclick(name,price)} title="add to cart"/>
-            </div>
-          )
-        }
-export default memo(Product)
+function Product({ image, exist, name, price, onclick }: IProducts) {
+  return (
+    <div className="border rounded-2xl p-3 m-3 place-content-center place-items-center">
+      <img src={image} alt="s" />
+      <p>{name}</p>
+      <p>{price}</p>
+      <p>{exist}</p>
+      <CustomButton1 onClick={() => onclick(name, price)} title="add to cart" />
+    </div>
+  );
+}
+export default memo(Product);
