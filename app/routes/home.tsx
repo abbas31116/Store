16969a9555395c63027478aa70060897580b1ee8
Abbas <absrzamoh@gmail.com>
2025-09-29 17:08:33 +0330

    ...

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,14 +1,6 @@
-import axios from "axios";
-import { useContext, useEffect, useReducer, useState } from "react";
-import { initialState, reducer, type Post } from "./reducer";
-import { AccContext } from "./providers/AccountProviders";
-import { CustomInput } from "~/components/CustomInput";
-import CustomButton1 from "~/components/CustomButton";
-import { useCounterStore } from "~/zustand/counter_store";
-import { useUserStore } from "~/zustand/user_store";
-import { useFormik } from "formik";
-import { UserCartStore } from "~/zustand/cart_store";
+
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 interface IPost {
   title: string;
@@ -18,29 +10,56 @@ interface IPost {
 }
 
 const fetchPost = async (): Promise<IPost[]> => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/postss");
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   if (!res.ok) {
     throw new Error("network error");
   }
   return res.json();
 };
-
+interface IComment {
+  postId: number,
+  id: number,
+  name: string,
+  email: string,
+  body: string,
+}
+const comment = async (): Promise<IComment[]> => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/comments")
+  if (!res) {
+    throw new Error("network")
+  }
+  return res.data
+}
 export default function Home() {
-  const { isLoading, isError, data, error } = useQuery({
-    queryKey: ["POST"],
-    queryFn: fetchPost,
-    staleTime: 1000 * 60,
-    gcTime: 1000 * 60 * 5,
+  const { data: Comment, error: errComment, isError: isErrComment, isLoading: isLoadingComment } = useQuery({
+    queryKey: ["COMMENTS"],
+    queryFn: comment,
   });
-  return isLoading ? (
-    <div>loading</div>
-  ) : isError ? (
-    <div>{error.message}</div>
-  ) : (
-    <div>
-      {data?.map((item, index) => (
-        <p key={index}>{item.title}</p>
-      ))}
-    </div>
-  );
+  return isLoadingComment?(
+    <div>loading data...</div>
+  ):isErrComment?(
+    <div>{errComment.message}</div>
+  )
+  :(<div>
+    {Comment?.map((item,index)=>(
+      <p key={index}>{item.id}</p>
+    ))}
+  </div>)
+  // const { isLoading, isError, data, error } = useQuery({
+  //   queryKey: ["POST"],
+  //   queryFn: fetchPost,
+  //   staleTime: 1000 * 60,
+  //   gcTime: 1000 * 60 * 5,
+  // });
+  // return isLoading ? (
+  //   <div>loading</div>
+  // ) : isError ? (
+  //   <div>{error.message}</div>
+  // ) : (
+  //   <div>
+  //     {data?.map((item, index) => (
+  //       <p key={index}>{item.title}</p>
+  //     ))}
+  //   </div>
+  // );
 }
